2980d48f90ecaa4dcb63ad10221debfec9d83052
sushilshakya-github <sushilkumar.shakya@gmail.com>
2024-09-05 20:49:12 +0530

    Maven based build
    Initial commit

---
+++ src/main/java/com/audition/service/AuditionService.java
@@ -0,0 +1,35 @@
+package com.audition.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.audition.integration.AuditionIntegrationClient;
+import com.audition.model.AuditionComment;
+import com.audition.model.AuditionPost;
+
+@Service
+public class AuditionService {
+
+    @Autowired
+    private AuditionIntegrationClient auditionIntegrationClient;
+
+
+    public List<AuditionPost> getPosts() {
+        return auditionIntegrationClient.getPosts();
+    }
+
+    public AuditionPost getPostById(final String postId) {
+        return auditionIntegrationClient.getPostById(postId);
+    }
+
+    public List<AuditionComment> getPostComments(String postId) {
+        return auditionIntegrationClient.getComments(postId);
+    }
+
+    public List<AuditionComment> getCommentbyPostId(final String postId) {
+        return auditionIntegrationClient.getCommentByPostId(postId);
+    }
+
+}
